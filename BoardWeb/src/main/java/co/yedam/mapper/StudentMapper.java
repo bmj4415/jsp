ea86676fee0057e2dd30bcb5141659a22846d563
bmj4415 <bmj4415@gmail.com>
2024-06-11 00:12:15 +0900

    Update StudentMapper.java

--- BoardWeb/src/main/java/co/yedam/mapper/StudentMapper.java
+++ BoardWeb/src/main/java/co/yedam/mapper/StudentMapper.java
@@ -8,6 +8,7 @@ public interface StudentMapper {
 	public List<Student> selectBlog(); // 반환되는 타입들을 여러가지 만들기 위해 리스트형식으로하고 반환타입을 co.yedam.vo.Student이 아닌 Student로
 	public int insertStudent(Student student);
 	public int updateStudent(Student student);
+	public int deleteStudent(Student student);
 	
 	
 }//
